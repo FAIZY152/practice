f66c2fc9a4fa57cd54aec53cdff940d14c9c583e
Muhammad Fayaz <141697766+FAIZY152@users.noreply.github.com>
2025-03-06 00:03:54 +0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,27 @@ const nextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: "/api/:path*", // Apply to all API routes
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*", // Allow all origins (Change to frontend URL in production)
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
